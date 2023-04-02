d09d680d2f66abcfe553ff08ed94a29f045270b1
Mingming <peiliming910@gmail.com>
2023-04-03 00:26:09 +0900

    post用のmdファイルを移動&fsモジュールでmdファイルを参照と読み込み

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,4 +1,6 @@
 import { NextResponse } from 'next/server';
+import fs from 'fs'
+import path from 'path'
 // import { NextApiHandler } from 'next'
 
 // const handler: NextApiHandler = (req, res) => {
@@ -19,13 +21,23 @@ import { NextResponse } from 'next/server';
 export async function GET(request: Request, response: NextResponse) {
   const { method } = request
 
-  const data = { ok: true }
-
   switch(method) {
-    case 'POST': 
+    case 'GET': {
+      const data = readPostsInfo()
       return NextResponse.json(data)
+    }
     default:
       return NextResponse.json({status: 404})
   }
 
+}
+
+const readPostsInfo = () => {
+  const dirPathToRead = path.join(process.cwd(), 'posts')
+  const dirs = fs.readdirSync(dirPathToRead)
+  dirs.map((filename) => {
+    const filePathToRead = path.join(process.cwd(), 'posts/' + filename)
+    const fileContent = fs.readFileSync(filePathToRead, {encoding: 'utf-8'})
+    console.log(fileContent)
+  })
 }
