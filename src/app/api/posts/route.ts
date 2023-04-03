f3a25d05d1467c4c4fa5fa4a59f24b0b80653d8e
Mingming <peiliming910@gmail.com>
2023-04-04 00:40:08 +0900

    next.js13 クライアント側でgetStaticPropsが使えなくなって、fixing...

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -2,22 +2,12 @@ import { NextResponse } from 'next/server';
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
-// import { NextApiHandler } from 'next'
-
-// const handler: NextApiHandler = (req, res) => {
-//   console.log(req.method)
-
-//   res.json({ ok: true })
-// }
-
-// export default handler
 
 // GET
 // POST
 // PATCH
 // PUT
 // DELETE
-
 
 export async function GET(request: Request, response: NextResponse) {
   const { method } = request
--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,27 +1,40 @@
-'use client'
+// 'use client'
 
-import { NextPage } from 'next'
+import { NextPage, InferGetStaticPropsType } from 'next'
+import { GetServerSideProps } from 'next'
 import { useState, useEffect } from 'react'
 import BlogCard from '@/app/components/BlogCard'
 
+interface PostApiResponse {
+  postInfo: {
+    title: string
+    slug: string
+    meta: string
+  }[]
+}
+
+// export const getStaticProps = async () => {
+//     const { postInfo }: PostApiResponse = await fetch('http://localhost:3000/api/posts').then(data => data.json())
+
+//   return {
+//     props: { posts: postInfo }
+//   }
+// }
+
+export async function getData() {
+  const res = await fetch('http://localhost:3000/api/posts')
+  return res.json()
+}
+
+//type Props = InferGetStaticPropsType<typeof getData>
 interface Props {}
 
-const Blogs: NextPage<Props> = () => {
-  const [ posts, setPosts ] = useState<
-    {title: string; slug: string; meta: string }[]
-  >([])
-
-  const fetchPosts = async () => {
-    const res = await fetch('/api/posts').then(data => data.json())
-    setPosts(res.postInfo)
-  }
-
-  useEffect(() => {
-    fetchPosts()
-  }, [])
+const Blogs: NextPage<Props> = ({ posts }) => {
   return (
     <div className="max-w-3xl mx-auto p-5 space-y-5">
-      {posts.map((post, index) => <BlogCard key={index} title={post.title} desc={post.meta} />)}
+      {posts.map((post) => (
+        <BlogCard title={post.title} desc={post.meta} />)
+      )}
     </div>
   )
 }
