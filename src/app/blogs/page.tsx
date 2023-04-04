7aad63794b9df0976337b97f49327dc35e775aa3
Mingming <peiliming910@gmail.com>
2023-04-04 23:30:50 +0900

    next.js13のfetch()でAPIデータ取得修正

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,4 +1,4 @@
-// 'use client'
+//'use client'
 
 import { NextPage, InferGetStaticPropsType } from 'next'
 import { GetServerSideProps } from 'next'
@@ -22,21 +22,19 @@ interface PostApiResponse {
 // }
 
 export async function getData() {
-  const res = await fetch('http://localhost:3000/api/posts')
+  const res = await fetch('http://localhost:3000/api/posts', { cache: 'force-cache' })
   return res.json()
 }
+const { postInfo }: PostApiResponse = await getData()
+// type Props = InferGetStaticPropsType<typeof getData>
+interface Props{}
 
-const data: PostApiResponse = await getData()
-
-type Props = InferGetStaticPropsType<typeof data>
-// interface Props {
-// }
-
-const Blogs: NextPage<Props> = ({ data }) => {
+const Blogs: NextPage<Props> = async () => {
+  //const { postInfo }: PostApiResponse = await getData()
   return (
     <div className="max-w-3xl mx-auto p-5 space-y-5">
-      {posts.map((post) => (
-        <BlogCard title={post.title} desc={post.meta} />)
+      {postInfo.map((post, index) => (
+        <BlogCard key={index} title={post.title} desc={post.meta} />)
       )}
     </div>
   )
