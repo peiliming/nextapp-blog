04626d0754e66d0e373071d525a0208ce359df66
Mingming <peiliming910@gmail.com>
2023-04-05 00:33:38 +0900

    getStaticPaths()が廃止され、generateStaticParams()を使う

--- src/app/blogs/[postSlug]/page.tsx
+++ src/app/blogs/[postSlug]/page.tsx
@@ -1,7 +1,16 @@
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
 
 interface Props {}
+
+export async function generateStaticParams() { // getStaticPaths()の置き換え
+  return [
+    { postSlug: 'clever-lazy-loading-for-javascript' }
+  ]
+}
 
 const SinglePage: NextPage<Props> = () => {
   // const router = useRouter()
