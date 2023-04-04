f3e3911b7e6c95c88f5d36c5a7c9eae0b78a5291
Mingming <peiliming910@gmail.com>
2023-04-04 23:52:22 +0900

    各文章のslugを設定

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -8,8 +8,8 @@ import BlogCard from '@/app/components/BlogCard'
 interface PostApiResponse {
   postInfo: {
     title: string
-    slug: string
     meta: string
+    slug: string
   }[]
 }
 
@@ -20,11 +20,11 @@ export async function getData() {
 const { postInfo }: PostApiResponse = await getData()
 interface Props{}
 
-const Blogs: NextPage<PostApiResponse> = async () => {
+const Blogs: NextPage<Props> = async () => {
   return (
     <div className="max-w-3xl mx-auto p-5 space-y-5">
       {postInfo.map((post, index) => (
-        <BlogCard key={index} title={post.title} desc={post.meta} />)
+        <BlogCard key={post.slug} title={post.title} desc={post.meta} slug={post.slug} />)
       )}
     </div>
   )
--- src/app/components/BlogCard.tsx
+++ src/app/components/BlogCard.tsx
@@ -1,16 +1,20 @@
 import { FC } from 'react'
+import Link from 'next/link'
 
 interface Props {
   title: string
   desc: string
+  slug: string
 }
 
-const BlogCard: FC<Props> = ({title, desc}): JSX.Element => {
+const BlogCard: FC<Props> = ({title, desc, slug}): JSX.Element => {
   return (
-    <div className="bg-green-100 p-2">
-      <h1 className="text-gray-900 text-3xl font-semibold">{title}</h1>
-      <p className="text-gray-500">{desc}</p>
-    </div>
+    <Link href={'/blogs/' + slug} className="block">
+      <div className="bg-green-100 p-2 rounded cursor-pointer">
+        <h1 className="text-gray-900 text-3xl font-semibold">{title}</h1>
+        <p className="text-gray-500">{desc}</p>
+      </div>
+    </Link>
   )
 }
 
