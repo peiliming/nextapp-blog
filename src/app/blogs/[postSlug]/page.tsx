ea30834b131199db25d751320ad34513ea8b93ab
Mingming <peiliming910@gmail.com>
2023-04-05 00:09:23 +0900

    動的routeファイルを作成（v13でslug名をフォルダーに付けるようになった）

---
+++ src/app/blogs/[postSlug]/page.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next'
+import { useRouter } from 'next/router'
+
+interface Props {}
+
+const SinglePage: NextPage<Props> = () => {
+  // const router = useRouter()
+  // console.log(router)
+  return (<div>SinglePage</div>)
+}
+
+export default SinglePage
