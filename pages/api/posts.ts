8a66ffed29a53f5555997243b24358fc2e3718f4
Mingming <peiliming910@gmail.com>
2023-04-06 21:24:48 +0900

    node.jsのfs、pathでpostAPIを作成

--- pages/api/posts.ts
+++ pages/api/posts.ts
@@ -1,15 +1,32 @@
 import { NextApiHandler } from 'next'
+import fs from 'fs'
+import path from 'path'
 
 const handler: NextApiHandler = (req, res) => {
   const { method } = req
 
   switch(method) {
-    case 'GET':
-      return res.json({ok: true})
+    case 'GET': {
+      const data = readPostsInfo()
+      return res.json({postInfo: data})
+    }
+      
     default:
       return res.status(404).send('Not Found')
   }
 
+}
+
+const readPostsInfo = () => {
+  const dirPathToRead = path.join(process.cwd(), 'posts')
+  const dirs = fs.readdirSync(dirPathToRead)
+  dirs.map((filename) => {
+    const filePathToRead = path.join(process.cwd(), `posts/${filename}`)
+    const fileContent = fs.readFileSync(filePathToRead, {encoding: 'utf-8'})
+    console.log(fileContent)
+  })
+
+  return ''
 }
 
 export default handler
